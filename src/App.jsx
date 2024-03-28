35cf67448a7ef92c27673a70f864e429973f76ef
KejuuS <azkalazkia30@gmail.com>
2024-03-28 16:42:52 +0700

    uploaded to github

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Profile from "./assets/Profile.jpg";
+import { List } from "./components/LinkList";
+
+const App = () => {
+  return (
+    <div>
+      <div className="container flex items-center justify-center w-full h-screen">
+        <div className="bg-darkGrey w-[375px] h-[430px] rounded-lg">
+          <div className="flex flex-col items-center justify-start">
+            <div className="flex justify-center items-center mt-5 mb-2 w-[100px] overflow-hidden rounded-full object-center">
+              <img src={Profile} alt="" className="w-[200px]" />
+            </div>
+
+            <div className="my-3 text-center">
+              <h1 className="text-xl font-bold text-white">Muhammad Azkal Azkia</h1>
+              <p className="mb-4 text-sm font-semibold text-primary">
+                Jakarta, Indonesia
+              </p>
+
+              <p className="text-sm text-white font-base">
+                "A passionate frontend developer from Jakarta"
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center my-3">
+              {List.map(({ href, title, img }) => (
+                <a
+                  href={href}
+                  target="_blank"
+                  className="group cursor-pointer grid grid-cols-2 justify-center  items-start w-[250px]  py-2 px-10 bg-grey rounded-lg mb-5 hover:bg-primary duration-100:"
+                  key={href}
+                >
+                  <div className="flex items-center justify-center">
+                    <img src={img} alt="" className="w-[20px]" />
+                  </div>
+
+                  <li
+                    className="group-hover:text-offBlack list-none text-sm font-semibold text-white group-[hover:text-offblack] tracking-normal pr-10"
+                    key={href}
+                  >
+                    {title}
+                  </li>
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/LinkList.jsx
@@ -0,0 +1,24 @@
+import Instagram from "../assets/instagram.png";
+import LinkedIn from "../assets/linkedin.png";
+import Github from "../assets/github-mark.png";
+
+export const List = [
+  {
+    title: "Instagram",
+    href: "https://www.instagram.com/azkalazkiaaa",
+    img: Instagram,
+  },
+
+  {
+    title: "LinkedIn",
+    href: "https://www.linkedin.com/in/azkalazkia/",
+    img: LinkedIn,
+  },
+
+  {
+    title: "Github",
+    href: "https://github.com/KejuuS",
+    img: Github,
+  },
+
+];
